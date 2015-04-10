0deaa3bbd3da2914904569d47c042044111afd34
benega <thiagobenega@gmail.com>
2015-04-09 22:07:28 -0300

    Teste

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends ActionBarActivity {
 
             // Create some dummy data for the ListView.  Here's a sample weekly forecast
             String[] data = {
-                    "Mon 6/23 - Sunny - 31/17",
+                    "Mon 6/23 - Sunny - 31/17 ",
                     "Tue 6/24 - Foggy - 21/8",
                     "Wed 6/25 - Cloudy - 22/17",
                     "Thurs 6/26 - Rainy - 18/11",
@@ -76,13 +76,13 @@ public class MainActivity extends ActionBarActivity {
                     "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
                     "Sun 6/29 - Sunny - 20/7"
             };
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+            List<String> weekForecast = new ArrayList<>(Arrays.asList(data));
 
             // Now that we have some dummy forecast data, create an ArrayAdapter.
             // The ArrayAdapter will take data from a source (like our dummy forecast) and
             // use it to populate the ListView it's attached to.
             ArrayAdapter<String> forecastAdapter =
-                    new ArrayAdapter<String>(
+                    new ArrayAdapter<>(
                             getActivity(), // The current context (this activity)
                             R.layout.list_item_forecast, // The name of the layout ID.
                             R.id.list_item_forecast_textview, // The ID of the textview to populate.
